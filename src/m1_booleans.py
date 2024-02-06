2e6ef6a7b131b1c5d5d84c9c9e66b1a1dfde8a9c
michaelkuznicki <mkuznicki@franciscan.edu>
2024-02-06 08:52:17 -0500

    Finished m1 and m2

---
+++ src/m1_booleans.py
@@ -0,0 +1,25 @@
+###############################################################################
+# TODO: 1. (3 pts)
+#
+#   Consider each of the objects below. Before writing any code, predicter
+#   whether each one evaluates to True or False.
+#
+#   Now write a line of code for each one that prints the boolean evaluation of
+#   each object. Remember, we get the boolean evaluation of an object using the
+#   function bool()
+#
+#   5
+#   "False"
+#   "True"
+#   ""
+#   0
+#   -1
+#   False
+#   True
+#   2.0
+#   7 > 4
+#   5 == 4
+#   3 > 4
+#
+#   Once you have done this, then change the above _TODO_ to DONE.
+###############################################################################
---
+++ src/m2_operators.py
@@ -0,0 +1,55 @@
+txt = "The quick brown fox jumps over the lazy dog."
+t1 = "fox"
+t2 = "cat"
+
+###############################################################################
+# TODO: 1. (6 pts)
+#
+#   Write each of the functions below (each that takes two parameters and uses
+#   the appropriate operator from the reading) that simply returns the boolean
+#   evaluation of those two parameters:
+#
+#   equal()
+#   not_equal()
+#   greater_than()
+#   less_than()
+#   greater_than_or_equal_to()
+#   less_than_or_equal_to()
+#
+#   Now write a line of code for each one using numbers for the parameters that
+#   would cause each function to return True. 
+#   
+#   Once you have done this, then change the above _TODO_ to DONE.
+###############################################################################
+
+###############################################################################
+# TODO: 1. (2 pts)
+#
+#   Write a line of code that returns True if the string
+#       t1 (defined above)
+#   is contained in the string
+#       txt (also defined above)
+#   and prints the result.
+#
+#   Write another line of code that returns True if the string
+#       t2
+#   is contained in the string
+#       txt
+#   and prints the result.
+#
+#   Run your code. Does it return what you expect?
+#
+#   Once you have done this, then change the above _TODO_ to DONE.
+###############################################################################
+
+###############################################################################
+# TODO: 1. (1 pt)
+#
+#   Now, write a line of code that returns True if the string
+#       t1
+#   is *not* the same thing as
+#       t2
+#   and prints the result
+#
+#   Once you have done this, then change the above _TODO_ to DONE.
+###############################################################################
